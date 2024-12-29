d6e9b8a5b45af0c7f39d0ca936f66243dabd6ff2
darellanodev <darellano.aliaga@gmail.com>
2024-12-29 20:29:42 +0100

    Fix eslint config to work with typescript (#3)

--- eslint.config.js
+++ eslint.config.js
@@ -3,36 +3,34 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import tsParser from "@typescript-eslint/parser";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
+import * as tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default [
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      "plugin:@typescript-eslint/recommended",
-      "plugin:react/recommended",
-      "plugin:react-hooks/recommended",
-    ],
-    files: ["**/*.{ts,tsx}"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    ignores: ["dist/**", "node_modules/**", "pnpm-lock.yaml"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
-      parser: tsParser,
+      sourceType: "module",
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.eslint.json",
+      },
+      globals: {
+        ...globals.browser,
+      },
     },
     plugins: {
-      "@typescript-eslint": tsPlugin,
-      react: react.configs.recommended.rules,
+      "@typescript-eslint": tseslint.plugin,
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      ...tsPlugin.configs.recommended.rules,
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
       ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
@@ -44,7 +42,4 @@ export default tseslint.config(
       },
     },
   },
-  {
-    ignores: ["node_modules/", "dist/", "pnpm-lock.yaml"],
-  }
-);
+];
