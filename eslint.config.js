52d1fee0e9deeb38ed504ddc0d19626c3fc4b01d
darellanodev <darellano.aliaga@gmail.com>
2024-12-29 20:11:57 +0100

    Add the configuration for eslint to work with react in eslint.config.js (#3)

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
@@ -13,6 +14,8 @@ export default tseslint.config(
       js.configs.recommended,
       ...tseslint.configs.recommended,
       "plugin:@typescript-eslint/recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
     ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
@@ -22,16 +25,23 @@ export default tseslint.config(
     },
     plugins: {
       "@typescript-eslint": tsPlugin,
+      react: react.configs.recommended.rules,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       ...tsPlugin.configs.recommended.rules,
+      ...react.configs.recommended.rules,
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
   {
