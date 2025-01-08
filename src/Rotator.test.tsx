fc83fe3002da04096783d78b5cc62e3b9f21b09e
darellanodev <darellano.aliaga@gmail.com>
2025-01-08 19:57:31 +0100

    Pass a list of images and display the first image (#11)

--- src/Rotator.test.tsx
+++ src/Rotator.test.tsx
@@ -1,9 +1,34 @@
 import { render, screen } from '@testing-library/react'
 import Rotator from './Rotator'
 
+const images = [
+  {
+    imgUrl: './assets/image1.png',
+    description: 'Image 1',
+    author: 'Author 1',
+  },
+  {
+    imgUrl: './assets/image2.png',
+    description: 'Image 2',
+    author: 'Author 2',
+  },
+  {
+    imgUrl: './assets/image3.png',
+    description: 'Image 3',
+    author: 'Author 3',
+  },
+]
+
 describe('Rotator', () => {
   it('renders a <div>', () => {
     render(<Rotator />)
     expect(screen.getByTestId('rotator')).toBeInTheDocument()
+  })
+  it('renders the first image by default', () => {
+    render(<Rotator images={images} />)
+    expect(screen.getByRole('img')).toHaveAttribute(
+      'src',
+      './assets/image1.png',
+    )
   })
 })
--- src/Rotator.tsx
+++ src/Rotator.tsx
@@ -1,5 +1,18 @@
-const Rotator = () => {
-  return <div data-testid="rotator" />
+import { ReactNode } from 'react'
+import RotatorImage from './RotatorImage'
+
+type Image = {
+  imgUrl?: string
+  description?: ReactNode
+  author?: ReactNode
+}
+
+const Rotator = ({ images }: { images?: Image[] }) => {
+  return (
+    <div data-testid="rotator">
+      <RotatorImage {...images?.[0]} />
+    </div>
+  )
 }
 
 export default Rotator
