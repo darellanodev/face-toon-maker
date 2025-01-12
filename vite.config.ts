87e61f4843f91dab50c26a2b89507aed6c5414b2
darellanodev <darellano.aliaga@gmail.com>
2025-01-12 18:23:01 +0100

    Configure the vite.config.ts to use the babel plugin styled components (closes #14)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,21 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: true,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
   test: {
     globals: true,
     environment: 'happy-dom',
