7587e6ffae1bb3a0531d9f6a6feeedad82d4b266
darellanodev <darellano.aliaga@gmail.com>
2025-01-17 20:42:30 +0100

    Move the src/assets/img folder to the public/img folder (#17)

--- src/images.tsx
+++ src/images.tsx
@@ -1,16 +1,16 @@
 export default [
   {
-    imgUrl: './src/assets/img/image1.png',
+    imgUrl: './img/image1.png',
     description: 'Image 1',
     author: 'Author 1',
   },
   {
-    imgUrl: './src/assets/img/image2.png',
+    imgUrl: './img/image2.png',
     description: 'Image 2',
     author: 'Author 2',
   },
   {
-    imgUrl: './src/assets/img/image3.png',
+    imgUrl: './img/image3.png',
     description: 'Image 3',
     author: 'Author 3',
   },
