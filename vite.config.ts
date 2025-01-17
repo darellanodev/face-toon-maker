36ba1af78fffe8c9285e2be8c0f1ddc49a79dd2d
darellanodev <darellano.aliaga@gmail.com>
2025-01-17 20:15:44 +0100

    Separate the vite.config.ts and the vitest.config.ts (#17)

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -17,9 +17,4 @@ export default defineConfig({
       },
     }),
   ],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: ['test-setup.ts'],
-  },
 })
