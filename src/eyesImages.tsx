94b5eac1aefbc0662761916c2f119db229338cf0
darellanodev <darellano.aliaga@gmail.com>
2025-01-25 20:04:10 +0100

    Move the eyes images into the img/eyes folder (#22)

--- src/eyesImages.tsx
+++ src/eyesImages.tsx
@@ -1,16 +1,16 @@
 export default [
   {
-    imgUrl: './img/image1.png',
+    imgUrl: './img/eyes/image1.png',
     description: 'Image 1',
     author: 'Author 1',
   },
   {
-    imgUrl: './img/image2.png',
+    imgUrl: './img/eyes/image2.png',
     description: 'Image 2',
     author: 'Author 2',
   },
   {
-    imgUrl: './img/image3.png',
+    imgUrl: './img/eyes/image3.png',
     description: 'Image 3',
     author: 'Author 3',
   },
